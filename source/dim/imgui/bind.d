21ab0a20d8e92e1ea9371b3fe3965f6c86863257
bggd <gameovera@live.com>
2020-05-13 22:47:55 +0900

    Fix invalid type

--- source/dim/imgui/bind.d
+++ source/dim/imgui/bind.d
@@ -29,6 +29,11 @@ void StyleColorsLight(ImGuiStyle* dst = null);
 
 void End();
 
+void Image(ImTextureID user_texture_id, ref const(ImVec2) size,
+        ref const(ImVec2) uv0 = ImVec2(0, 0).byRef(), ref const(ImVec2) uv1 = ImVec2(1,
+            1).byRef(), ref const(ImVec4) tint_col = ImVec4(1, 1, 1, 1).byRef(),
+        ref const(ImVec4) border_col = ImVec4(0, 0, 0, 0).byRef());
+
 private:
 
 bool Begin(const(char)* name, bool* p_open = null, int flags = 0);
